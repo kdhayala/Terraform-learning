90feabc2c811974987169ecc80b114671aae8585
kumar <kumardhayait15@gmail.com>
2022-12-14 12:05:03 +0530

    commit1

--- 02-variable/var.tf
+++ 02-variable/var.tf
@@ -9,3 +9,45 @@ output "sample" {
 output "sample1" {
   value = "Hey,${var.sample}"
 }
+
+
+#string Data types
+
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+#Boolean data types
+variable "sample3" {
+  default = true
+}
+
+#List varibale
+
+variable "sample4" {
+  default = [
+     "hello",
+    100,
+    true
+  ]
+}
+
+#map variable
+ variable "sample5" {
+   default = {
+     string = "hello"
+     number = 100
+     boolean = true
+   }
+ }
+
+
+#### access the list value
+output "sample4" {
+  value = var.sample4
+}
