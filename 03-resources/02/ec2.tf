afbc680177f049a4daaa04036dc4f3d3373984c1
kumar <kumardhayait15@gmail.com>
2022-12-20 22:58:15 +0530

    [31] Adding a Hello World Script | kumar students.

--- 03-resources/02/ec2.tf
+++ 03-resources/02/ec2.tf
@@ -7,4 +7,11 @@ provider "aws" {
 resource "aws_instance" "sample" {
   ami = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
+  root_block_device {
+    volume_size = 15
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.sample.public_ip
 }
