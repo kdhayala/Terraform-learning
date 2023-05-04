dda402c1535d4ee49a681d6009a395a10e42a475
kumar <kumardhayait15@gmail.com>
2023-05-04 16:37:34 +0530

    functions

--- 03-resources/03/sg.tf
+++ 03-resources/03/sg.tf
@@ -16,10 +16,9 @@ resource "aws_security_group" "allow_tls" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "allow_ssh"
   }
 }
---
+++ 05-functions/main.tf
@@ -0,0 +1,25 @@
+output "sample" {
+  value = element(var.sample, 0)
+}
+
+variable "sample" {
+  default = ["abc", 123]
+}
+
+variable "sample1" {
+  default = {
+    orange =  {
+      name = "Orange"
+      count = 10
+    }
+    apple = {
+      name = "apple"
+      count = 100
+    }
+  }
+}
+
+
+output "sample1" {
+  value = lookup(lookup(var.sample1, "apple", null), "count", 0v)
+}
